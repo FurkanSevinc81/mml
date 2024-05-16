59a5fc2ea0151454a1989d78a11f88f055b602b8
Furkan Sevinc <furkan_sevinc81@hotmail.de>
2024-05-16 22:08:35 +0200

    added meta data

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+from setuptools import setup, find_packages
 setup(name='mml',
       version='1.0',
+      description='multimodal machine learning for pain recognition module',
+      author='Furkan Sevinc',
       packages=find_packages())
